215e35fcc50ebca82fff678f76966939ee3b7a29
Maks <124205758+MaksioPiksio@users.noreply.github.com>
2023-11-23 15:42:24 +0100

    Update assets.js

--- src/assets.js
+++ src/assets.js
@@ -2,6 +2,6 @@ export const skins = [921379, 886606, 38148, 45237, 779175, 769121, 763236, 7591
 export const buyPriceArray = [5.61, 5.85, 7.85, 3.19, 3.65, 3.43, 3.76, 4.27, 3.46, 5.71, 3.61, 3.41, 3.38, 1.94, 1.53, 1.55, 1.55, 1.45, 1.75, 0, 8.13, 9.07, 2.59, 3.23, 49.72, 47.49, 18, 8.65, 3.40, 8.19, 9.67, 13.5, 5.52 , 11.85 , 18.08, 16 , 19.47, 12.8 , 17.9 ]; /*prettier-ignore */
 export const numberOfSkins = [1, 2, 3, 4, 1, 2, 2, 2, 2, 9, 10, 8, 10, 1, 6, 4, 1, 26, 30, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2]; /*prettier-ignore */
 
-export const headers = ["Number", "icon", "name", "buyPrice", "buffPrice", "steamPrice", "quantity", "profit"]; /*prettier-ignore */
+export const headers = ["", "icon", "name", "buyPrice", "buffPrice", "steamPrice", "quantity", "profit"]; /*prettier-ignore */
 export const currencyArray = [" RMB", " USD", " PLN"];
 export const currencyPriceObject = { " RMB": 1, " USD": 0.15, " PLN": 0.58 };
