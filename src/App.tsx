85eb65a486bc81fd3f20491d4e4ad569b6cd84c2
Maks <124205758+MaksioPiksio@users.noreply.github.com>
2024-01-30 10:05:23 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -113,7 +113,7 @@ function App() {
             <a
                 href="https://www.youtube.com/watch?v=gMuDEDIPZ3c"
                 className="flex items-center justify-center text-white">
-                <img src="public/Bob.png" alt="Potwierdzenie że paryż pójdzie w góre" />
+                <img src="Bob.png" alt="Potwierdzenie że paryż pójdzie w góre" />
             </a>
         </>
     );
